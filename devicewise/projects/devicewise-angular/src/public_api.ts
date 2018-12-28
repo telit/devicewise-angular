c6504e81d85ca07b0c5f07eae025f9e9b4f73e0d
Austen Stone <miltonlaxer@gmail.com>
2018-12-27 19:51:27 -0500

    cleanup

--- devicewise/projects/devicewise-angular/src/public_api.ts
+++ devicewise/projects/devicewise-angular/src/public_api.ts
@@ -3,5 +3,8 @@
  */
 
 export * from './lib/devicewise-angular.service';
+export * as Dwrequest from './lib/models/dwrequest';
+export * from './lib/models/dwresponse';
+export * from './lib/models/dwsubscription';
 export * from './lib/devicewise-angular.component';
 export * from './lib/devicewise-angular.module';
